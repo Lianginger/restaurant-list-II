022103cbf607f418cba6e55f5a2d9e35701929e7
lianginger <jackyliang444@hotmail.com.tw>
2019-04-13 11:23:40 +0800

    Add edit page, update action and delete action

--- app.js
+++ app.js
@@ -28,9 +28,11 @@ db.once('open', function () {
 // 處理請求與回應
 // 列出所有餐廳
 app.get('/', (req, res) => {
-  Restaurant.find((err, restaurants) => {
-    res.render('index', { restaurants })
-  })
+  Restaurant.find()
+    .sort({ _id: -1 })
+    .exec((err, restaurants) => {
+      res.render('index', { restaurants })
+    })
 })
 
 // 建立新餐廳資料頁面
@@ -67,17 +69,40 @@ app.get('/restaurants/:id', (req, res) => {
 
 // 編輯特定餐廳頁面
 app.get('/restaurants/:id/edit', (req, res) => {
-  res.send('編輯特定餐廳頁面')
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    res.render('edit', { restaurant })
+  })
 })
 
 // 編輯特定餐廳
 app.post('/restaurants/:id/edit', (req, res) => {
-  res.send('編輯特定餐廳')
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    restaurant.name = req.body.name
+    restaurant.category = req.body.category
+    restaurant.image = req.body.image
+    restaurant.location = req.body.location
+    restaurant.phone = req.body.phone
+    restaurant.google_map = req.body.google_map
+    restaurant.rating = req.body.rating
+    restaurant.escription = req.body.description
+
+    restaurant.save((err) => {
+      if (err) return console.error(err)
+      res.redirect('/restaurants/' + req.params.id)
+    })
+  })
 })
 
 // 刪除特定餐廳
 app.post('/restaurants/:id/delete', (req, res) => {
-  res.send('刪除特定餐廳')
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    restaurant.remove((err) => {
+      if (err) return console.error(err)
+      res.redirect('/')
+    })
+  })
 })
 
 // 啟動並監聽
