5d264333c5266d2094214fc0d3acc8e4de5fca7c
lianginger <jackyliang444@hotmail.com.tw>
2019-05-01 19:46:22 +0800

    Refact the auth.js to prevent nest function

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,5 @@
 module.exports = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next()
-  } else {
-    req.flash('warning_msg', '請先登入才能使用')
-    res.redirect('/users/login')
-  }
+  if (req.isAuthenticated()) return next()
+  req.flash('warning_msg', '請先登入才能使用')
+  res.redirect('/users/login')
 }
